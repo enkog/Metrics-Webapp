88912ac8870df2cf8ba60eac9b27b4befc79e4bd
enkog <nkirukaogu@gmail.com>
2021-10-12 15:35:48 +0100

    Install axios and fetch data from api

--- src/store/countriesSlice.js
+++ src/store/countriesSlice.js
@@ -1,4 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchCountries = createAsyncThunk('countries/fetchCountries', async () => {
+  const response = await axios.get('https://restcountries.com/v3.1/all');
+  return response.data;
+});
 
 const countriesSlice = createSlice({
   name: 'countries',
